7c7bb9cc09abb6142d2b70bdce9ee6e7c1397ce6
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2006-08-28 20:32:36 +0000

    r37: - Checkpoint
    - Fix module usage count

--- dm-convergent/convergent.c
+++ dm-convergent/convergent.c
@@ -7,50 +7,78 @@
 extern char *svn_branch;
 extern char *svn_revision;
 
-#define log(prio, msg, args...) printk(prio "dm-convergent: " msg, ## args)
+struct convergent_dev {
+	struct dm_dev *dmdev;
+	unsigned blocksize;
+	unsigned offset;
+};
+
+#define log(prio, msg, args...) printk(prio "dm-convergent: " msg "\n", ## args)
 #define debug(msg, args...) log(KERN_DEBUG, msg, args)
 
 /* argument format: blocksize backdev backdevoffset */
 /* XXX how does backdevoffset work if our own starting offset is nonzero? */
-static int convergent_target_ctr(struct dm_target *target,
+static int convergent_target_ctr(struct dm_target *ti,
 				unsigned int argc, char **argv)
 {
+	struct convergent_dev *dev;
 	char *endp;
-	unsigned blocksize;
-	unsigned offset;
-
+	int ret;
+	
 	if (argc != 3) {
-		target->error="convergent: invalid arguments: should be " \
+		ti->error="convergent: invalid arguments: should be " \
 				"<blocksize> <backing-device> " \
 				"<backing-device-offset>";
 		return -EINVAL;
 	}
-
-	blocksize=simple_strtoul(argv[0], &endp, 10);
-	/* XXX hardsect_size maximum is 65536! */
-	if (*endp != 0 || blocksize % 512 != 0 ||
-				blocksize > (unsigned short)-1) {
-		target->error="convergent: block size must be multiple of 512 and <= 65536";
-		return -EINVAL;
-	}
-	target->limits.hardsect_size=blocksize;
 	
-	/* XXX offset could be long long */
-	offset=simple_strtoul(argv[2], &endp, 10);
-	if (*endp != 0) {
-		target->error="convergent: invalid backing device offset";
-		return -EINVAL;
+	dev=kzalloc(sizeof(*dev), GFP_KERNEL);
+	if (dev == NULL) {
+		ti->error="convergent: could not allocate memory";
+		return -ENOMEM;
 	}
-	debug("blocksize %u, backdev %s, offset %u\n", blocksize, argv[1], offset);
+	ti->private=dev;
+	
+	dev->blocksize=simple_strtoul(argv[0], &endp, 10);
+	/* XXX hardsect_size maximum is 65536! */
+	if (*endp != 0 || dev->blocksize % 512 != 0 ||
+				dev->blocksize > (unsigned short)-1) {
+		ti->error="convergent: block size must be multiple of 512 and <= 65536";
+		ret=-EINVAL;
+		goto out;
+	}
+	ti->limits.hardsect_size=dev->blocksize;
+	/* XXX offset could be long long */
+	dev->offset=simple_strtoul(argv[2], &endp, 10);
+	if (*endp != 0) {
+		ti->error="convergent: invalid backing device offset";
+		ret=-EINVAL;
+		goto out;
+	}
+	debug("blocksize %u, backdev %s, offset %u", dev->blocksize,
+				argv[1], dev->offset);
 	
 	/* XXX variable offset size depending on large device support */
-//	dm_get_device(target, argv[1], offset, 
-	return -EINVAL;
+	/* XXX perhaps ti->table->mode? */
+	ret=dm_get_device(ti, argv[1], dev->offset, ti->len,
+				FMODE_READ|FMODE_WRITE, &dev->dmdev);
+	if (ret) {
+		ti->error="convergent: could not get backing device";
+		goto out;
+	}
+	
+	return 0;
+out:
+	kfree(dev);
+	return ret;
 }
 
-static void convergent_target_dtr(struct dm_target *target)
+static void convergent_target_dtr(struct dm_target *ti)
 {
+	struct convergent_dev *dev=ti->private;
 	
+	dm_put_device(ti, dev->dmdev);
+	kfree(dev);
 }
 
 static int convergent_map(struct dm_target *target, struct bio *bio,
@@ -61,6 +89,7 @@ static int convergent_map(struct dm_target *target, struct bio *bio,
 
 static struct target_type convergent_target = {
 	.name =		"convergent",
+	.module =	THIS_MODULE,
 	.version =	{0,0,0}, /* XXX */
 	.ctr =		convergent_target_ctr,
 	.dtr =		convergent_target_dtr,
@@ -70,8 +99,7 @@ static struct target_type convergent_target = {
 static int __init convergent_init(void)
 {
 	int ret;
-	printk(KERN_INFO "dm-convergent: starting (%s, rev %s)\n",
-			svn_branch, svn_revision);
+	log(KERN_INFO, "loading (%s, rev %s)", svn_branch, svn_revision);
 	ret=dm_register_target(&convergent_target);
 	if (ret)
 		log(KERN_ERR, "convergent registration failed: %d", ret);
@@ -81,6 +109,7 @@ static int __init convergent_init(void)
 static void __exit convergent_shutdown(void)
 {
 	int ret;
+	log(KERN_INFO, "unloading");
 	ret=dm_unregister_target(&convergent_target);
 	if (ret)
 		log(KERN_ERR, "convergent unregistration failed: %d", ret);
