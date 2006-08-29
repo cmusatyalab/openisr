dd54aae6f35275dc80d07fc32b09148aa0a43a76
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2006-08-29 12:56:10 +0000

    r38: - Include private header drivers/md/dm.h.  I had tried to avoid this, but not everything is in device-mapper.h that needs to be.
    - Fixes for large block device support
    
    - Module description macros
    
    - Working map function

--- dm-convergent/convergent.c
+++ dm-convergent/convergent.c
@@ -3,6 +3,8 @@
 #include <linux/kernel.h>
 #include <linux/bio.h>
 #include <linux/device-mapper.h>
+/* We need this for the definition of struct dm_dev. */
+#include "dm.h"
 
 extern char *svn_branch;
 extern char *svn_revision;
@@ -10,14 +12,19 @@ extern char *svn_revision;
 struct convergent_dev {
 	struct dm_dev *dmdev;
 	unsigned blocksize;
-	unsigned offset;
+	sector_t offset;
 };
+
+#ifdef CONFIG_LBD
+#define simple_strtosector simple_strtoull
+#else
+#define simple_strtosector simple_strtoul
+#endif
 
 #define log(prio, msg, args...) printk(prio "dm-convergent: " msg "\n", ## args)
 #define debug(msg, args...) log(KERN_DEBUG, msg, args)
 
 /* argument format: blocksize backdev backdevoffset */
-/* XXX how does backdevoffset work if our own starting offset is nonzero? */
 static int convergent_target_ctr(struct dm_target *ti,
 				unsigned int argc, char **argv)
 {
@@ -48,17 +55,15 @@ static int convergent_target_ctr(struct dm_target *ti,
 		goto out;
 	}
 	ti->limits.hardsect_size=dev->blocksize;
-	/* XXX offset could be long long */
-	dev->offset=simple_strtoul(argv[2], &endp, 10);
+	dev->offset=simple_strtosector(argv[2], &endp, 10);
 	if (*endp != 0) {
 		ti->error="convergent: invalid backing device offset";
 		ret=-EINVAL;
 		goto out;
 	}
-	debug("blocksize %u, backdev %s, offset %u", dev->blocksize,
-				argv[1], dev->offset);
+	debug("blocksize %u, backdev %s, offset " SECTOR_FORMAT,
+				dev->blocksize, argv[1], dev->offset);
 	
-	/* XXX variable offset size depending on large device support */
 	/* XXX perhaps ti->table->mode? */
 	ret=dm_get_device(ti, argv[1], dev->offset, ti->len,
 				FMODE_READ|FMODE_WRITE, &dev->dmdev);
@@ -81,10 +86,14 @@ static void convergent_target_dtr(struct dm_target *ti)
 	kfree(dev);
 }
 
-static int convergent_map(struct dm_target *target, struct bio *bio,
+static int convergent_map(struct dm_target *ti, struct bio *bio,
 				union map_info *map_context)
 {
-	return -EINVAL;
+	struct convergent_dev *dev=ti->private;
+	
+	bio->bi_bdev=dev->dmdev->bdev;
+	bio->bi_sector=(bio->bi_sector - ti->begin) + dev->offset;
+	return 1;
 }
 
 static struct target_type convergent_target = {
@@ -117,10 +126,9 @@ static void __exit convergent_shutdown(void)
 
 module_init(convergent_init);
 module_exit(convergent_shutdown);
-#if 0
-/* XXX */
-MODULE_AUTHOR("Benjamin Gilbert <bgilbert@cs.cmu.edu>");
-MODULE_DESCRIPTION(DM_NAME " target for convergent encryption and compression");
-MODULE_LICENSE();
-#endif
 
+MODULE_AUTHOR("Benjamin Gilbert <bgilbert@cs.cmu.edu>");
+MODULE_DESCRIPTION("device-mapper target for convergent encryption "
+			"and compression");
+/* We must use a GPL-compatible license to use the crypto API */
+MODULE_LICENSE("GPL");
