ed6c93dabf3c7e85ca3d32f05e518c52fbdfa559
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-01-08 19:00:53 +0000

    r399: - Fix races between thread_start() and cpu_callback()
    - Minor restructuring in cpu_start()/cpu_stop()
    
    - Move some debug messages from cpu_callback to cpu_start/stop
    
    - Cancel CPU hot-unplug if this is the last CPU on which we have a kernel
    thread.  (Note: triggers kernel bug involving failure to release locks
    if hot-unplug is canceled)

--- nexus/thread.c
+++ nexus/thread.c
@@ -7,7 +7,9 @@
 
 /* XXX percpu vars */
 static struct {
+	MUTEX lock;
 	struct task_struct *task[NR_CPUS];
+	int count;
 } threads;
 
 static struct {
@@ -66,33 +68,38 @@ static int nexus_thread(void *data)
 
 static int cpu_start(int cpu)
 {
-	int err;
+	struct task_struct *thr;
 	
-	BUG_ON(threads.task[cpu] != NULL);
-	threads.task[cpu]=kthread_create(nexus_thread, NULL,
-				KTHREAD_NAME "/%d", cpu);
-	if (IS_ERR(threads.task[cpu])) {
-		err=PTR_ERR(threads.task[cpu]);
-		threads.task[cpu]=NULL;
-		return err;
-	}
-	kthread_bind(threads.task[cpu], cpu);
+	BUG_ON(!mutex_is_locked(&threads.lock));
+	if (threads.task[cpu] != NULL)
+		return 0;  /* See comment in cpu_callback() */
+	
+	debug("Onlining %d", cpu);
+	thr=kthread_create(nexus_thread, NULL, KTHREAD_NAME "/%d", cpu);
+	if (IS_ERR(thr))
+		return PTR_ERR(thr);
+	threads.task[cpu]=thr;
+	threads.count++;
+	kthread_bind(thr, cpu);
 	/* Make sure the thread doesn't have a higher priority than interactive
 	   processes (e.g. the X server) because they'll become somewhat
 	   less interactive under high I/O load */
-	set_user_nice(threads.task[cpu], 0);
-	wake_up_process(threads.task[cpu]);
+	set_user_nice(thr, 0);
+	wake_up_process(thr);
 	return 0;
 }
 
 static void cpu_stop(int cpu)
 {
-	if (threads.task[cpu]) {
-		debug("Calling kthread_stop on %d", cpu);
-		kthread_stop(threads.task[cpu]);
-		debug("...done");
-		threads.task[cpu]=NULL;
-	}
+	BUG_ON(!mutex_is_locked(&threads.lock));
+	if (threads.task[cpu] == NULL)
+		return;
+	
+	debug("Offlining %d", cpu);
+	kthread_stop(threads.task[cpu]);
+	debug("...done");
+	threads.task[cpu]=NULL;
+	threads.count--;
 }
 
 /* Runs in process context; can sleep */
@@ -101,19 +108,34 @@ static int cpu_callback(struct notifier_block *nb, unsigned long action,
 {
 	int cpu=(int)data;
 	
+	/* Due to the implementation of CPU hotplug, it is possible to receive
+	   CPU_ONLINE for cpus that thread_start() has already configured, or
+	   to receive CPU_DEAD for cpus we never started. */
+	mutex_lock(&threads.lock);
 	switch (action) {
 	case CPU_ONLINE:
 		/* CPU is already up */
-		debug("Onlining %d", cpu);
 		if (cpu_start(cpu))
 			log(KERN_ERR, "Failed to start thread for cpu %d", cpu);
 		break;
+	case CPU_DOWN_PREPARE:
+		debug("Preparing to offline %d", cpu);
+		if (threads.count == 1 && threads.task[cpu] != NULL) {
+			/* This is the last CPU on which we have a running
+			   thread, since we were unable to start a thread
+			   for a new CPU at some point in the past.  Cancel
+			   the shutdown. */
+			debug("...refusing");
+			mutex_unlock(&threads.lock);
+			return NOTIFY_BAD;
+		}
+		break;
 	case CPU_DEAD:
 		/* CPU is already down */
-		debug("Offlining %d", cpu);
 		cpu_stop(cpu);
 		break;
 	}
+	mutex_unlock(&threads.lock);
 	return NOTIFY_OK;
 }
 
@@ -143,24 +165,32 @@ int __init thread_start(void)
 	for (i=0; i<NR_CALLBACKS; i++)
 		INIT_LIST_HEAD(&queues.list[i]);
 	init_waitqueue_head(&queues.wq);
+	mutex_init(&threads.lock);
 	
-	lock_cpu_hotplug();
+	/* lock_cpu_hotplug() only protects the online cpu map; it doesn't
+	   prevent notifier callbacks from occurring.  threads.lock makes
+	   sure the callback can't run until we've finished initialization */
+	mutex_lock(&threads.lock);
 	err=register_cpu_notifier(&cpu_notifier);
 	if (err) {
-		unlock_cpu_hotplug();
+		mutex_unlock(&threads.lock);
 		return err;
 	}
+	
+	lock_cpu_hotplug();
 	for_each_online_cpu(cpu) {
 		err=cpu_start(cpu);
-		if (err)
+		if (err) {
+			unlock_cpu_hotplug();
 			goto bad;
+		}
 	}
 	unlock_cpu_hotplug();
+	mutex_unlock(&threads.lock);
 	return 0;
 	
 bad:
 	unregister_cpu_notifier(&cpu_notifier);
-	unlock_cpu_hotplug();
 	for_each_possible_cpu(cpu)
 		cpu_stop(cpu);
 	return err;
@@ -170,7 +200,9 @@ void __exit thread_shutdown(void)
 {
 	int cpu;
 	
+	mutex_lock(&threads.lock);
 	unregister_cpu_notifier(&cpu_notifier);
 	for_each_possible_cpu(cpu)
 		cpu_stop(cpu);
+	mutex_unlock(&threads.lock);
 }
