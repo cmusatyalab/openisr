5c2b7ce3466e97af72ef918e086a4fdf60f5a84f
Matt Toups <mtoups@cs.cmu.edu>
2007-01-29 22:39:05 +0000

    r455: beginnings of a debian/ package dir for 'openisr'
    so far only packages vulpes, more to come!

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the openisr package
+#
+0 4	* * *	root	openisr_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(openisr):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="openisr" command="/usr/bin/openisr"
