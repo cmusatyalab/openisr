23169ccb77d30d02c09e0a2cb7d9212c82ea2b31
Matt Toups <mtoups@cs.cmu.edu>
2007-01-30 02:39:39 +0000

    r456: oops, didn't mean to commit all of those examples

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the openisr package
-#
-0 4	* * *	root	openisr_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(openisr):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="openisr" command="/usr/bin/openisr"
