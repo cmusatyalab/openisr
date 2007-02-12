aaf2669e1bb931cb3c7d28d95d003ac9b10d8ef5
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-02-12 21:01:34 +0000

    r532: - Add license blocks to Nexus source files
    - Rename COPYING.GPL to LICENSE.GPL

--- nexus/thread.c
+++ nexus/thread.c
@@ -1,3 +1,21 @@
+/* 
+ * Nexus - convergently encrypting virtual disk driver for the OpenISR (TM)
+ *         system
+ * 
+ * Copyright (C) 2006-2007 Carnegie Mellon University
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.  A copy of the GNU General Public License
+ * should have been distributed along with this program in the file
+ * LICENSE.GPL.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
 #include <linux/kthread.h>
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
