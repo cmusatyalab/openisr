ccf8fe4c5fe2593475a41f581e263e7604548242
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-02-12 20:51:20 +0000

    r529: - libvdisk: Add Eclipse license block

--- libvdisk/libvdisk.c
+++ libvdisk/libvdisk.c
@@ -1,3 +1,14 @@
+/*
+ * libvdisk - LD_PRELOAD library for block device access from finicky VMMs
+ * 
+ * Copyright (C) 2006 Carnegie Mellon University
+ * 
+ * This software is distributed under the terms of the Eclipse Public License, 
+ * Version 1.0 which can be found in the file named LICENSE.Eclipse.  ANY USE, 
+ * REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES RECIPIENT'S 
+ * ACCEPTANCE OF THIS AGREEMENT
+ */
+
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
