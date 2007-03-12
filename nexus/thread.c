c3d146630d96bca215679cfa144aeb9be55d6593
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-03-12 17:12:29 +0000

    r676: - Add comment blocks to thread.c

--- nexus/thread.c
+++ nexus/thread.c
@@ -59,9 +59,30 @@ static struct task_struct *io_thread;
 static struct task_struct *request_thread;
 
 
-/* This will always run on the processor to which it is bound, *except* during
-   hot-unplug of that CPU (or in a corner case on <= 2.6.9), when it will run
-   on an arbitrary processor. */
+/**
+ * nexus_thread - thread function for per-CPU callback processor
+ * @data: pointer to &nexus_tfm_state for this thread
+ * 
+ * One of these will normally be running on each CPU in the system; this is
+ * the process context in which much of the Nexus code runs.  It is structured
+ * in terms of callbacks: Nexus code calls schedule_callback() passing
+ * a callback type and a pointer to a &list_head to be queued for that callback
+ * type.  nexus_thread() then runs callbacks in priority order.  Callback
+ * types are assigned a priority by their enumeration order in &enum callback;
+ * the thread will run all priority-zero callbacks before running any
+ * priority-one callbacks, and so on.  Starvation is prevented by the fact
+ * that the chunkdata state machine requires each chunk to proceed through each
+ * of the callbacks in some order; if only high-priority callbacks are
+ * processed, eventually every chunk in the chunkdata cache will be waiting for
+ * crypto and the low-priority crypto callback will be reached.  The priority
+ * order is chosen so that quick tasks, or those which start I/O, are
+ * high-priority, and crypto (which is long-running) is always the lowest
+ * priority.
+ * 
+ * nexus_thread() will always run on the specific processor to which it is
+ * bound, *except* during hot-unplug of that CPU (or in a corner case on
+ * <= 2.6.9), when it will run on an arbitrary processor.
+ **/
 static int nexus_thread(void *data)
 {
 	struct nexus_tfm_state *ts=data;
@@ -108,6 +129,13 @@ next:
 	return 0;
 }
 
+/**
+ * schedule_callback - arrange for a callback to be run as soon as possible
+ * @type : the callback type, indicating which callback function should be used
+ * @entry: the &list_head to be passed to the callback function
+ * 
+ * This must be safe to call from any context, including hardirq.
+ **/
 void schedule_callback(enum callback type, struct list_head *entry)
 {
 	unsigned long flags;
@@ -120,13 +148,19 @@ void schedule_callback(enum callback type, struct list_head *entry)
 	wake_up_interruptible(&queues.wq);
 }
 
-/* Helper thread to submit I/O.  We don't want to do this in the per-CPU
-   thread because it's allowed to block if there are already too many
-   outstanding requests to the chunk store, and we want to be able to continue
-   to do crypto and service other requests while we wait. */
-/* Technically we could spawn one thread per device so that a blocked queue
-   for one chunk store won't affect unrelated devices, but we have too many
-   threads already.  This can be changed later if it becomes a problem. */
+/**
+ * nexus_io_thread - helper thread to submit I/O to chunk store
+ * 
+ * This just loops calling generic_make_request().  We don't want to make
+ * these calls from the per-CPU thread because generic_make_request() is
+ * allowed to block if there are already too many outstanding requests to the
+ * chunk store, and we want to be able to continue to do crypto and service
+ * other requests while we wait.
+ * 
+ * Technically we could spawn one thread per device so that a blocked queue
+ * for one chunk store won't affect unrelated devices, but we have too many
+ * threads already.  This can be changed later if it becomes a problem.
+ **/
 static int nexus_io_thread(void *ignored)
 {
 	struct bio *bio;
@@ -156,10 +190,16 @@ static int nexus_io_thread(void *ignored)
 	return 0;
 }
 
-/* We don't want to have to do a memory allocation to queue I/O, so we need
-   to use the linked list mechanism already included in struct bio.
-   Unfortunately, this is just a "next" pointer rather than a
-   struct list_head. */
+/**
+ * schedule_io - arrange for @bio to be submitted later from process context
+ *
+ * This just queues bios for nexus_io_thread().  It is only safe to call from
+ * process context.
+ * 
+ * We don't want to have to do a memory allocation to queue I/O, so we need
+ * to use the linked list mechanism already included in struct bio.
+ * Unfortunately, this is just a "next" pointer rather than a &list_head.
+ **/
 void schedule_io(struct bio *bio)
 {
 	BUG_ON(bio->bi_next != NULL);
@@ -177,27 +217,30 @@ void schedule_io(struct bio *bio)
 	wake_up_interruptible(&pending_io.wq);
 }
 
-/* Helper thread to run request queues.  Request queues are different from
-   other types of callbacks: the request queue code needs to be able to return
-   callbacks to the head of the queue if an allocation failure occurs, and
-   this operation must always preserve queue order; it needs to be able to
-   delay walking the queue if there's an out-of-memory condition; and we
-   need to be able to process one dev's requests even if another dev is out of
-   chunkdata buffers.  Therefore, we use a two-stage queue walk: there's a
-   per-dev request list, and one callback processes the entire per-dev list at
-   once (in request.c).
-
-   In order to ensure that allocation failures do not reorder requests in a
-   particular dev's list, we must make sure that only one thread can process
-   a dev's request list at a time.  We could do this in the per-CPU crypto
-   threads using a per-dev lock, but then we'd have to choose between:
-   complex code, race conditions, or allowing crypto threads to uselessly
-   block on a dev mutex when they could be getting work done.  For simplicity,
-   therefore, we only allow one thread to be processing request queues at a
-   time.  There's no clean way to do that within the per-CPU thread
-   architecture, so we have a special singleton thread for this purpose.  This
-   is separate from the I/O thread because we still want to process incoming
-   requests even if our underlying chunk store's request queue is full. */
+/**
+ * nexus_request_thread - helper thread to run request queues
+ * 
+ * Request queues are different from other types of callbacks: the request
+ * queue code needs to be able to return callbacks to the head of the queue if
+ * an allocation failure occurs, and this operation must always preserve queue
+ * order; it needs to be able to delay walking the queue if there's an
+ * out-of-memory condition; and we need to be able to process one dev's
+ * requests even if another dev is out of chunkdata buffers.  Therefore, we use
+ * a two-stage queue walk: there's a per-dev request list, and one callback
+ * processes the entire per-dev list at once (in request.c).
+ * 
+ * In order to ensure that allocation failures do not reorder requests in a
+ * particular dev's list, we must make sure that only one thread can process
+ * a dev's request list at a time.  We could do this in the per-CPU crypto
+ * threads using a per-dev lock, but then we'd have to choose between
+ * complex code, race conditions, or allowing crypto threads to uselessly
+ * block on a dev mutex when they could be getting work done.  For simplicity,
+ * therefore, we only allow one thread to be processing request queues at a
+ * time.  There's no clean way to do that within the per-CPU thread
+ * architecture, so we have a special singleton thread for this purpose.  This
+ * is separate from the I/O thread because we still want to process incoming
+ * requests even if our underlying chunk store's request queue is full.
+ **/
 static int nexus_request_thread(void *ignored)
 {
 	struct list_head *entry;
@@ -223,6 +266,11 @@ static int nexus_request_thread(void *ignored)
 	return 0;
 }
 
+/**
+ * schedule_request_callback - arrange for a request callback to run later
+ *
+ * This can safely be called from any context.
+ **/
 void schedule_request_callback(struct list_head *entry)
 {
 	unsigned long flags;
@@ -234,7 +282,11 @@ void schedule_request_callback(struct list_head *entry)
 	wake_up_interruptible(&pending_requests.wq);
 }
 
-/* Only for debug use via sysfs */
+/**
+ * wake_all_threads - make all Nexus threads runnable
+ *
+ * Only for debug use via sysfs.
+ **/
 void wake_all_threads(void)
 {
 	log(KERN_NOTICE, "Unwedging threads");
@@ -245,6 +297,16 @@ void wake_all_threads(void)
 
 #define nexus_suite nexus_crypto
 
+/**
+ * alloc_TYPE_on_all - allocate transform structures on every running CPU
+ * @TYPE: "suite" or "compress"
+ * @arg: suite or compression algorithm to be allocated
+ *
+ * Allocates data structures for processing the given suite or compression
+ * algorithm on every running CPU.  The suite or algorithm must not already
+ * be allocated.  On allocation failure, backs out the allocation and returns
+ * an error code.  Thread lock must be held.
+ **/
 #define DEFINE_ALLOC_ON_ALL(TYPE)					\
 static int alloc_##TYPE##_on_all(enum nexus_##TYPE arg)			\
 {									\
@@ -276,6 +338,13 @@ bad:									\
 	return err;							\
 }
 
+/**
+ * free_TYPE_on_all - free transform structures on every running CPU
+ * @TYPE: "suite" or "compress"
+ * @arg: suite or compression algorithm to be freed
+ *
+ * Thread lock must be held.
+ **/
 #define DEFINE_FREE_ON_ALL(TYPE)					\
 static void free_##TYPE##_on_all(enum nexus_##TYPE arg)			\
 {									\
@@ -303,6 +372,13 @@ DEFINE_FREE_ON_ALL(compress)
 #undef DEFINE_FREE_ON_ALL
 #undef nexus_suite
 
+/**
+ * alloc_all_on_cpu - allocate suite and compress structures for new CPU
+ * 
+ * For each suite or compression algorithm which is currently allocated
+ * on the other CPUs, performs the same allocation for CPU @cpu.  Backs out
+ * allocations on failure.  Thread lock must be held.
+ **/
 static int alloc_all_on_cpu(int cpu)
 {
 	enum nexus_crypto suite=0;
@@ -347,6 +423,11 @@ bad:
 	return err;
 }
 
+/**
+ * free_all_on_cpu - free all suite and compress structures from @cpu
+ *
+ * Thread lock must be held.
+ **/
 static void free_all_on_cpu(int cpu)
 {
 	enum nexus_crypto suite;
@@ -372,6 +453,13 @@ static void free_all_on_cpu(int cpu)
 				"cpu %d", suite_count, alg_count, cpu);
 }
 
+/**
+ * thread_register - allocate per-thread tfm structures for @dev
+ *
+ * Validates the transforms associated with @dev and registers them with the
+ * thread code.  A successful return indicates that all per-CPU threads will
+ * be able to process the transforms required by @dev.
+ **/
 int thread_register(struct nexus_dev *dev)
 {
 	enum nexus_compress alg;
@@ -426,6 +514,13 @@ bad:
 	return err;
 }
 
+/**
+ * thread_unregister - release tfm registration for @dev
+ *
+ * This function reverses the effects of thread_register().  This is safe
+ * to call even if thread_register() has not been called on this @dev or did
+ * not complete successfully.
+ **/
 void thread_unregister(struct nexus_dev *dev)
 {
 	enum nexus_compress alg;
@@ -451,6 +546,12 @@ void thread_unregister(struct nexus_dev *dev)
 	mutex_unlock(&threads.lock);
 }
 
+/**
+ * cpu_start - allocate data structures and start a new thread for @cpu
+ *
+ * If a per-CPU thread is already running on @cpu, cpu_start() will return
+ * success and take no other action.  Thread lock must be held.
+ **/
 static int cpu_start(int cpu)
 {
 	struct task_struct *thr;
@@ -486,6 +587,12 @@ static int cpu_start(int cpu)
 	return 0;
 }
 
+/**
+ * cpu_stop - kill the per-CPU thread on @cpu and free its data structures
+ *
+ * This is safe to call if no thread is running on @cpu.  Thread lock must
+ * be held.
+ **/
 static void cpu_stop(int cpu)
 {
 	BUG_ON(!mutex_is_locked(&threads.lock));
@@ -500,13 +607,42 @@ static void cpu_stop(int cpu)
 	threads.count--;
 }
 
-/* Runs in process context; can sleep */
+/**
+ * cpu_callback - CPU hotplug notification handler
+ *
+ * The CPU hotplug code calls this to let us know when CPUs come and go.  Runs
+ * in process context, and is permitted to sleep.
+ *
+ * Any running thread must be able to handle any transform which is associated
+ * with any nexus_dev.  If we encounter allocation failures when bringing up
+ * a new CPU, cpu_start() will refuse to start the thread.  This may cause some
+ * running CPUs not to have associated threads.
+ *
+ * We do not tear down a per-CPU thread until after its CPU has been shut
+ * down.  In the meantime, the kthread code will unbind the thread so that
+ * it can be scheduled on any processor.
+ *
+ * We will try to prevent the kernel from hot-unplugging any CPU which is
+ * running our only per-CPU thread.  (This can only occur if previous CPU
+ * hot-adds have failed.)  If the kernel completes the hot-unplug anyway,
+ * we will print a warning and refuse to stop the thread.  In this case,
+ * the thread will persist, runnable on any CPU, until module unload or until
+ * another plug-unplug cycle occurs on the thread's original CPU.  The
+ * thread will continue to use the downed CPU's data structure, and will
+ * compete for CPU time with any threads which are (subsequently) properly
+ * bound to particular CPUs.  However, correctness should not be compromised.
+ *
+ * Returning %NOTIFY_BAD to %CPU_DOWN_PREPARE may oops some kernels; apparently
+ * that part of the API was never well-tested.  If this becomes a problem
+ * in practice, we can change the DOWN_PREPARE handling to fall back on the
+ * unbound-thread solution for the affected kernels.
+ **/
 static int cpu_callback(struct notifier_block *nb, unsigned long action,
 			void *data)
 {
 	int cpu=(long)data;
 	
-	/* Any of these handlers may run before thread_start has actually
+	/* Any of these handlers may run before thread_start() has actually
 	   started any threads, so they must not make assumptions about the
 	   state of the system. */
 	mutex_lock(&threads.lock);
@@ -519,10 +655,6 @@ static int cpu_callback(struct notifier_block *nb, unsigned long action,
 	case CPU_DOWN_PREPARE:
 		/* This is only called for >= 2.6.10 */
 		if (threads.count == 1 && threads.task[cpu] != NULL) {
-			/* This is the last CPU on which we have a running
-			   thread, since we were unable to start a thread
-			   for a new CPU at some point in the past.  Cancel
-			   the shutdown. */
 			log(KERN_ERR, "Refusing to stop CPU %d: it is running "
 						"our last worker thread", cpu);
 			mutex_unlock(&threads.lock);
@@ -533,15 +665,7 @@ static int cpu_callback(struct notifier_block *nb, unsigned long action,
 		/* CPU is already down */
 		if (threads.count == 1) {
 			/* CPU_DOWN_PREPARE will prevent this for kernels
-			   >= 2.6.10.  For older kernels, we kludge: just
-			   allow the thread to keep running.  It will continue
-			   to use the downed CPU's data structure but will
-			   run on any available processor.  If more hotplug
-			   events follow, this thread will compete for CPU time
-			   with any thread which is *supposed* to be running
-			   on a given CPU.  However, correctness should not
-			   be compromised.  This is not worth fixing
-			   properly. */
+			   >= 2.6.10. */
 			log(KERN_NOTICE, "Disabled CPU %d, which was running "
 						"our last worker thread", cpu);
 			log(KERN_NOTICE, "Leaving "KTHREAD_NAME"/%d running "
@@ -570,6 +694,12 @@ static struct notifier_block cpu_notifier = {
 struct notifier_block *__dummy_nb=&cpu_notifier;
 #endif
 
+/**
+ * thread_shutdown - module de-initialization for thread code
+ *
+ * Stops per-CPU and singleton threads, if running.  Called from thread_start()
+ * error path.
+ **/
 void thread_shutdown(void)
 {
 	int cpu;
@@ -596,6 +726,12 @@ void thread_shutdown(void)
 	}
 }
 
+/**
+ * thread_start - module initialization for thread code
+ *
+ * Starts per-CPU and singleton threads.  Backs out and returns error if any
+ * thread fails to start.
+ **/
 int __init thread_start(void)
 {
 	struct task_struct *thr;
