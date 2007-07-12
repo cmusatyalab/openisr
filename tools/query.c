0471233cf3cbfdc94583cfdafbb70613e0a05e8e
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-07-12 14:02:50 -0400

    query: Perform queries within a transaction
    - Perform queries within a transaction
    
    - Improve error handling

--- tools/query.c
+++ tools/query.c
@@ -4,13 +4,13 @@
 #include <unistd.h>
 #include <sqlite3.h>
 
+#define RETRY_USECS 5000
+
 static sqlite3 *db;
 
-static void __attribute__ ((noreturn)) sqlerr(char *prefix)
+static void sqlerr(char *prefix)
 {
 	fprintf(stderr, "%s: %s\n", prefix, sqlite3_errmsg(db));
-	sqlite3_close(db);
-	exit(1);
 }
 
 static void __attribute__ ((noreturn)) die(char *str, ...)
@@ -21,8 +21,6 @@ static void __attribute__ ((noreturn)) die(char *str, ...)
 	vfprintf(stderr, str, ap);
 	fprintf(stderr, "\n");
 	va_end(ap);
-	if (db)
-		sqlite3_close(db);
 	exit(1);
 }
 
@@ -70,14 +68,30 @@ int main(int argc, char **argv)
 	sqlite3_stmt *stmt;
 	const char *query;
 	int ret;
+	int i=0;
+	int j;
 
 	if (argc != 3)
-		die("Usage: %s database query\n", argv[0]);
-	if (sqlite3_open(argv[1], &db))
+		die("Usage: %s database query", argv[0]);
+	if (sqlite3_open(argv[1], &db)) {
 		sqlerr("Opening database");
+		exit(1);
+	}
+retry:
+	if (i++ >= 10) {
+		sqlite3_close(db);
+		die("Retries exceeded");
+	}
+	if (sqlite3_exec(db, "BEGIN", NULL, NULL, NULL)) {
+		sqlerr("Beginning transaction");
+		sqlite3_close(db);
+		exit(1);
+	}
 	for (query=argv[2]; *query; ) {
-		if (sqlite3_prepare(db, query, -1, &stmt, &query))
+		if (sqlite3_prepare(db, query, -1, &stmt, &query)) {
 			sqlerr("Preparing query");
+			goto rollback;
+		}
 		while (1) {
 			ret=sqlite3_step(stmt);
 			switch (ret) {
@@ -86,20 +100,40 @@ int main(int argc, char **argv)
 				break;
 			case SQLITE_DONE:
 				goto out;
-			case SQLITE_BUSY:
-				/* XXX */
-				sleep(1);
-				continue;
 			default:
-				/* XXX */
-				break;
+				sqlerr("Executing query");
+				/* Fall through */
+			case SQLITE_BUSY:
+				sqlite3_finalize(stmt);
+				goto rollback;
 			}
 		}
 out:
-		if (sqlite3_finalize(stmt))
-			sqlerr("Freeing query");
+		sqlite3_finalize(stmt);
+	}
+	for (j=0; ; j++) {
+		if (j >= 50)
+			goto rollback;
+		ret=sqlite3_exec(db, "COMMIT", NULL, NULL, NULL);
+		if (ret == SQLITE_BUSY) {
+			usleep(RETRY_USECS);
+			continue;
+		} else if (ret) {
+			sqlerr("Committing transaction");
+			goto rollback;
+		} else {
+			break;
+		}
 	}
 	if (sqlite3_close(db))
 		sqlerr("Closing database");
 	return 0;
+
+rollback:
+	if (sqlite3_exec(db, "ROLLBACK", NULL, NULL, NULL)) {
+		sqlerr("Rolling back transaction");
+		sqlite3_close(db);
+		exit(1);
+	}
+	goto retry;
 }
