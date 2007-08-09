fdf00c6a759f93240eddcf89fc989e79dc18fc17
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-08-09 11:29:05 -0400

    pk: Run SQLite integrity check on keyring during validation
    - Run "PRAGMA integrity_check" on keyring in validate_keyring()

--- parcelkeeper/cache.c
+++ parcelkeeper/cache.c
@@ -459,6 +459,7 @@ int copy_for_upload(void)
 int validate_keyring(void)
 {
 	sqlite3_stmt *stmt;
+	const char *result;
 	unsigned expected_chunk=0;
 	unsigned chunk;
 	unsigned taglen;
@@ -469,6 +470,19 @@ int validate_keyring(void)
 
 	pk_log(LOG_INFO, "Validating keyring");
 	printf("Validating keyring...\n");
+	if (query(&stmt, state.db, "PRAGMA integrity_check", NULL) !=
+				SQLITE_ROW) {
+		query_free(stmt);
+		pk_log(LOG_ERROR, "Couldn't run SQLite integrity check");
+		return 1;
+	}
+	query_row(stmt, "s", &result);
+	sret=strcmp(result, "ok");
+	query_free(stmt);
+	if (sret) {
+		pk_log(LOG_ERROR, "SQLite integrity check failed");
+		return 1;
+	}
 	for (sret=query(&stmt, state.db, "SELECT chunk, tag, key, compression "
 				"FROM keys ORDER BY chunk ASC", NULL);
 				sret == SQLITE_ROW; sret=query_next(stmt)) {
@@ -537,6 +551,7 @@ int validate_cache(void)
 	pk_log(LOG_INFO, "Checking cache consistency");
 	printf("Checking local cache for internal consistency...\n");
 
+	/* XXX PRAGMA integrity_check on attached database? */
 	/* XXX transaction? */
 	if (query(&stmt, state.db, "SELECT count(*) FROM cache.chunks", NULL)
 				!= SQLITE_ROW) {
