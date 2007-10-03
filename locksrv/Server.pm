b2dabe00397303308d6115b54e04365ef63931c3
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-10-03 15:26:17 -0400

    locksrv: Update license blocks
    - Update copyright date on license blocks
    
    - Add license block to Server.pm

--- locksrv/Server.pm
+++ locksrv/Server.pm
@@ -2,6 +2,19 @@
 # Server.pm - Module for server scripts
 ############################################################
 
+#
+#                       Internet Suspend/Resume
+#           A system for capture and transport of PC state
+#
+#              Copyright (c) 2002-2004, Intel Corporation
+#         Copyright (c) 2004-2007, Carnegie Mellon University
+#
+# This software is distributed under the terms of the Eclipse Public
+# License, Version 1.0 which can be found in the file named LICENSE.Eclipse.
+# ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+# RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+#
+
 package Server;
 use POSIX;
 
