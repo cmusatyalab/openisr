cmusatyalab/openisr

commit 24536b8031de658a561cced5cda4b01c7aed28f2

pk: Add "crypto" column to hoard.chunks
- Add column to hoard.chunks giving the crypto suite with which the chunk
was hashed.  This column is equal to zero for free slots and is set to a
valid cryptotype for used slots.  Update hoard_put_chunk() to set this
column.  Update all code which deallocates chunks to zero out this column.

- Bump hoard index version

- Validate the chunks.crypto column in check_hoard(), and check the length
of each row's tag against the expected tag length for the row's crypto
suite.  Add crypto_is_valid() utility function.

- Use chunks.crypto in check_hoard_data() rather than hardcoding
CRY_AES_SHA1

- Remove tests from check_hoard_data() which check for things that
check_hoard() has already validated

- Bump calctag buffer in check_hoard_data() to 64 bytes

- Minor copyediting in create_hoard_index()

parcelkeeper/hoard.c
@@ -16,7 +16,7 @@
 #include <string.h>
 #include "defs.h"
 
-#define HOARD_INDEX_VERSION 4
+#define HOARD_INDEX_VERSION 5
 #define EXPAND_CHUNKS 128
 
 static pk_err_t create_hoard_index(void)
@@ -42,8 +42,9 @@ static pk_err_t create_hoard_index(void)
 				"tag BLOB UNIQUE, "
 				/* 512-byte sectors */
 				"offset INTEGER UNIQUE NOT NULL, "
-				"length INTEGER NOT NULL DEFAULT 0,"
-				"last_access INTEGER NOT NULL DEFAULT 0,"
+				"length INTEGER NOT NULL DEFAULT 0, "
+				"crypto INTEGER NOT NULL DEFAULT 0, "
+				"last_access INTEGER NOT NULL DEFAULT 0, "
 				"referenced INTEGER NOT NULL DEFAULT 0)",
 				NULL)) {
 		pk_log(LOG_ERROR, "Couldn't create chunk table");
@@ -161,8 +162,9 @@ static pk_err_t allocate_chunk_offset(int *offset)
 	}
 
 	if (query(NULL, state.hoard, "UPDATE chunks SET referenced = 1, "
-				"tag = NULL, length = 0, last_access = 0 "
-				"WHERE offset == ?", "d", *offset)) {
+				"tag = NULL, length = 0, crypto = 0, "
+				"last_access = 0 WHERE offset == ?", "d",
+				*offset)) {
 		pk_log(LOG_ERROR, "Couldn't allocate hoard cache chunk");
 		return PK_IOERR;
 	}
@@ -172,8 +174,9 @@ static pk_err_t allocate_chunk_offset(int *offset)
 static void deallocate_chunk_offset(int offset)
 {
 	if (query(NULL, state.hoard, "UPDATE chunks SET tag = NULL, "
-				"length = 0, last_access = 0, referenced = 0 "
-				"WHERE offset = ?", "d", offset)) {
+				"length = 0, crypto = 0, last_access = 0, "
+				"referenced = 0 WHERE offset = ?", "d",
+				offset)) {
 		pk_log(LOG_ERROR, "Couldn't deallocate hoard chunk at "
 					"offset %d", offset);
 	}
@@ -390,8 +393,10 @@ pk_err_t hoard_put_chunk(const void *tag, const void *buf, unsigned len)
 		return ret;
 	}
 	sret=query(NULL, state.hoard, "UPDATE chunks SET tag = ?, length = ?, "
-				"last_access = ? WHERE offset = ?", "bddd",
-				tag, parcel.hashlen, len, timestamp(), offset);
+				"crypto = ?, last_access = ? "
+				"WHERE offset = ?", "bddd",
+				tag, parcel.hashlen, len, parcel.crypto,
+				timestamp(), offset);
 	if (sret == SQLITE_CONSTRAINT) {
 		/* Someone else has already written this tag */
 		deallocate_chunk_offset(offset);

parcelkeeper/defs.h
@@ -261,6 +261,7 @@ pk_err_t parseuint(unsigned *out, char *in, int base);
 enum cryptotype parse_crypto(char *desc);
 enum compresstype parse_compress(char *desc);
 unsigned crypto_hashlen(enum cryptotype type);
+int crypto_is_valid(enum cryptotype type);
 int compress_is_valid(enum compresstype type);
 pk_err_t read_file(const char *path, char *buf, int *bufsize);
 pk_err_t read_sysfs_file(const char *path, char *buf, int bufsize);

parcelkeeper/hoard_modes.c
@@ -318,11 +318,11 @@ static pk_err_t check_hoard_data(void)
 	struct query *qry;
 	char buf[131072];  /* XXX assumes 128 KB */
 	const void *tag;
-	char calctag[20];  /* XXX */
-	unsigned expected_taglen;
+	char calctag[64];  /* XXX */
 	unsigned taglen;
 	unsigned offset;
 	unsigned len;
+	int crypto;
 	off64_t examined_bytes;
 	off64_t total_bytes;
 	int sret;
@@ -340,33 +340,15 @@ static pk_err_t check_hoard_data(void)
 	query_row(qry, "D", &total_bytes);
 	query_free(qry);
 
-	/* XXX shouldn't hardcode crypto type -- we need a column for this */
-	expected_taglen=crypto_hashlen(CRY_AES_SHA1);
 	examined_bytes=0;
-	for (sret=query(&qry, state.db, "SELECT tag, offset, length "
+	for (sret=query(&qry, state.db, "SELECT tag, offset, length, crypto "
 				"FROM temp.to_check", NULL);
 				sret == SQLITE_ROW; sret=query_next(qry)) {
-		query_row(qry, "bdd", &tag, &taglen, &offset, &len);
+		query_row(qry, "bddd", &tag, &taglen, &offset, &len, &crypto);
 		examined_bytes += len;
 		print_progress_mb(examined_bytes, total_bytes);
-		if (taglen != expected_taglen) {
-			/* XXX */
-			pk_log(LOG_ERROR, "Found chunk with invalid tag "
-						"length %d", taglen);
-			hoard_invalidate_chunk(offset, tag, taglen);
-			count++;
-			continue;
-		}
-		/* check_hoard() already checked this, but buffer overflows
-		   are bad */
-		if (len > sizeof(buf)) {
-			/* XXX */
-			pk_log(LOG_ERROR, "Found chunk with invalid length %d",
-						len);
-			hoard_invalidate_chunk(offset, tag, taglen);
-			count++;
-			continue;
-		}
+		/* We assume the taglen, crypto suite, and chunk length are
+		   good, because check_hoard() already validated these */
 		if (pread(state.hoard_fd, buf, len, ((off_t)offset) << 9)
 					!= (off_t)len) {
 			pk_log(LOG_ERROR, "Couldn't read chunk at offset %d",
@@ -376,7 +358,7 @@ static pk_err_t check_hoard_data(void)
 			continue;
 		}
 
-		if (digest(CRY_AES_SHA1, calctag, buf, len)) {
+		if (digest(crypto, calctag, buf, len)) {
 			pk_log(LOG_ERROR, "digest() failed");
 			ret=PK_CALLFAIL;
 			continue;
@@ -408,6 +390,9 @@ int check_hoard(void)
 	const char *uuid;
 	int offset;
 	int next_offset;
+	const void *tag;
+	unsigned taglen;
+	int crypto;
 	int count;
 	int sret;
 	int time;
@@ -464,17 +449,45 @@ int check_hoard(void)
 		goto bad;
 	}
 
+	count=0;
+	for (sret=query(&qry, state.db, "SELECT offset, tag, crypto FROM "
+				"hoard.chunks", NULL); sret == SQLITE_ROW;
+				sret=query_next(qry)) {
+		query_row(qry, "dbd", &offset, &tag, &taglen, &crypto);
+		if ((tag == NULL && crypto != 0) || (tag != NULL &&
+					(!crypto_is_valid(crypto) ||
+					crypto_hashlen(crypto) != taglen))) {
+			count++;
+			if (query(NULL, state.db, "UPDATE hoard.chunks "
+						"SET tag = NULL, length = 0, "
+						"crypto = 0, last_access = 0, "
+						"referenced = 0 WHERE "
+						"offset = ?", "d", offset))
+				goto bad;
+		}
+	}
+	query_free(qry);
+	if (sret != SQLITE_OK) {
+		pk_log(LOG_ERROR, "Couldn't query chunk list");
+		goto bad;
+	}
+	if (count)
+		pk_log(LOG_ERROR, "Cleaned %d chunks with invalid "
+					"crypto suite", count);
+
 	/* XXX assumes 128 KB */
 	if (cleanup_action(state.db, "UPDATE hoard.chunks SET tag = NULL, "
-				"length = 0, last_access = 0, referenced = 0 "
-				"WHERE length < 0 OR length > 131072 OR "
+				"length = 0, crypto = 0, last_access = 0, "
+				"referenced = 0 WHERE length < 0 OR "
+				"length > 131072 OR "
 				"(length == 0 AND tag NOTNULL)",
 				LOG_ERROR,
 				"chunks with invalid length"))
 		goto bad;
 	if (cleanup_action(state.db, "UPDATE hoard.chunks SET tag = NULL, "
-				"length = 0, last_access = 0, referenced = 0 "
-				"WHERE referenced != 0 AND referenced != 1",
+				"length = 0, crypto = 0, last_access = 0, "
+				"referenced = 0 WHERE referenced != 0 AND "
+				"referenced != 1",
 				LOG_ERROR,
 				"chunks with invalid referenced flag"))
 		goto bad;
@@ -518,7 +531,7 @@ int check_hoard(void)
 	   *now* while we still know it's consistent. */
 	if (config.flags & WANT_FULL_CHECK) {
 		if (query(NULL, state.db, "CREATE TEMP TABLE to_check AS "
-					"SELECT tag, offset, length "
+					"SELECT tag, offset, length, crypto "
 					"FROM hoard.chunks WHERE tag NOTNULL "
 					"ORDER BY offset", NULL)) {
 			pk_log(LOG_ERROR, "Couldn't enumerate hoarded chunks");

parcelkeeper/util.c
@@ -98,6 +98,18 @@ unsigned crypto_hashlen(enum cryptotype type)
 	return 0;
 }
 
+int crypto_is_valid(enum cryptotype type)
+{
+	switch (type) {
+	case CRY_AES_SHA1:
+	case CRY_BLOWFISH_SHA1:
+		return 1;
+	case CRY_UNKNOWN:
+		break;
+	}
+	return 0;
+}
+
 int compress_is_valid(enum compresstype type)
 {
 	if (type <= COMP_UNKNOWN ||

commit d07b73557e56496328c1fd8d89a9ffb53a471593

pk: Fix hoard cache corruption caused by commit 24536b803
- Fix bug in hoard_put_chunk() introduced by 24536b803.  The effect of
this bug was:

1.  (Likely) The metadata for newly written chunks would not be written
to the cache index, and the slots would be reclaimed as orphan by
hoard_try_cleanup(), or

2.  (Less likely) The metadata for newly written chunks would be written to
the wrong place in the cache index, causing the index to diverge from the
hoard cache. The originally-allocated slots would be reclaimed as orphan.

Users who ran affected revisions should run "isr checkhoard -c" after
updating.

parcelkeeper/hoard.c
@@ -394,7 +394,7 @@ pk_err_t hoard_put_chunk(const void *tag, const void *buf, unsigned len)
 	}
 	sret=query(NULL, state.hoard, "UPDATE chunks SET tag = ?, length = ?, "
 				"crypto = ?, last_access = ? "
-				"WHERE offset = ?", "bddd",
+				"WHERE offset = ?", "bdddd",
 				tag, parcel.hashlen, len, parcel.crypto,
 				timestamp(), offset);
 	if (sret == SQLITE_CONSTRAINT) {