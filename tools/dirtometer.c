a4f0025a66bbc32d458295ca9500cedd6841dd84
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2008-11-07 16:04:54 -0500

    dirtometer: Add statistics table
    - Add chunk access statistics table and update indicators to dirtometer
    
    - Add (non-functional) expander arrow for chunk bitmap

--- tools/dirtometer.c
+++ tools/dirtometer.c
@@ -34,8 +34,36 @@
 
 #define CONFIG_GROUP "dirtometer"
 
+const char *stats_headings_double[] = {
+	"Guest",
+	"Nexus",
+	"State",
+	NULL
+};
+
+const char *stats_headings_single[] = {
+	"Hit",
+	NULL
+};
+
+enum stats_labels {
+	SECTORS_READ,
+	SECTORS_WRITTEN,
+	CHUNK_READS,
+	CHUNK_WRITES,
+	CHUNKS_ACCESSED,
+	CHUNKS_DIRTIED,
+	CACHE_HIT_RATE,
+	NR_STATS
+};
+
+typedef long stats_array[NR_STATS][2];
+stats_array last_stats;
+GtkWidget *label[NR_STATS];
+
 GtkWidget *wd;
 GtkWidget *img;
+GtkWidget *expander;
 
 const char *uuid;
 const char *name;
@@ -47,15 +75,7 @@ GKeyFile *config;
 int state_fd;
 char *states;
 int numchunks;
-
-struct stats {
-	long sectors_read;
-	long sectors_written;
-	long chunk_reads;
-	long chunk_writes;
-	long cache_hits;
-	long cache_misses;
-} last_stats;
+int chunks_per_mb;
 
 void die(char *fmt, ...)
 {
@@ -155,22 +175,70 @@ long read_stat(char *attr)
 
 void update_stats(void)
 {
-	struct stats cur_stats = {
-		.sectors_read = read_stat("sectors_read"),
-		.sectors_written = read_stat("sectors_written"),
-		.chunk_reads = read_stat("chunk_reads"),
-		.chunk_writes = read_stat("chunk_writes"),
-		.cache_hits = read_stat("cache_hits"),
-		.cache_misses = read_stat("cache_misses"),
+	const GdkColor busy = {
+		.red = 65535,
+		.green = 0,
+		.blue = 0
 	};
+	stats_array cur_stats = {{0}};
+	int i;
+	int j;
+	char *str;
 
-	printf("sectors %ld %ld chunks %ld %ld cache %ld %ld\n",
-				cur_stats.sectors_read,
-				cur_stats.sectors_written,
-				cur_stats.chunk_reads,
-				cur_stats.chunk_writes,
-				cur_stats.cache_hits,
-				cur_stats.cache_misses);
+	cur_stats[SECTORS_READ][0] = read_stat("sectors_read");
+	cur_stats[SECTORS_WRITTEN][0] = read_stat("sectors_written");
+	cur_stats[CHUNK_READS][0] = read_stat("chunk_reads");
+	cur_stats[CHUNK_WRITES][0] = read_stat("chunk_writes");
+	cur_stats[CACHE_HIT_RATE][0] = read_stat("cache_hits");
+	cur_stats[CACHE_HIT_RATE][1] = cur_stats[CACHE_HIT_RATE][0] +
+				read_stat("cache_misses");
+	cur_stats[CHUNKS_ACCESSED][0] = 0;
+	cur_stats[CHUNKS_DIRTIED][0] = 0;
+	for (i = 0; i < numchunks; i++) {
+		if (states[i] & 0x8)
+			cur_stats[CHUNKS_DIRTIED][0]++;
+		if (states[i] & 0x4)
+			cur_stats[CHUNKS_ACCESSED][0]++;
+	}
+
+	/* If one or more of the stats were invalid, don't bother updating. */
+	for (i = 0; i < NR_STATS; i++)
+		for (j = 0; j < 2; j++)
+			if (cur_stats[i][j] == -1)
+				return;
+
+	for (i = 0; i < NR_STATS; i++) {
+		switch (i) {
+		case SECTORS_READ:
+		case SECTORS_WRITTEN:
+			str = g_strdup_printf("%.1f", 1.0 * cur_stats[i][0] /
+						(1 << 11));
+			break;
+		case CHUNK_READS:
+		case CHUNK_WRITES:
+		case CHUNKS_ACCESSED:
+		case CHUNKS_DIRTIED:
+			str = g_strdup_printf("%.1f", 1.0 * cur_stats[i][0] /
+						chunks_per_mb);
+			break;
+		case CACHE_HIT_RATE:
+			str = g_strdup_printf("%.1f%%", 100.0 * cur_stats[i][0]
+						/ cur_stats[i][1]);
+			break;
+		default:
+			str = g_strdup_printf("unknown");
+		}
+		gtk_label_set_label(GTK_LABEL(label[i]), str);
+		g_free(str);
+		if (last_stats[i][0] == cur_stats[i][0] || cur_stats[i][1])
+			gtk_widget_modify_fg(label[i], GTK_STATE_NORMAL,
+						NULL);
+		else
+			gtk_widget_modify_fg(label[i],	GTK_STATE_NORMAL,
+						&busy);
+		for (j = 0; j < 2; j++)
+			last_stats[i][j] = cur_stats[i][j];
+	}
 }
 
 void free_pixels(unsigned char *pixels, void *data)
@@ -324,15 +392,34 @@ void init_files(void)
 				linkparts[g_strv_length(linkparts) - 1]);
 	g_strfreev(linkparts);
 	g_free(path);
+
+	chunks_per_mb = read_stat("chunk_size");
+	if (chunks_per_mb == -1)
+		die("Couldn't get parcel chunk size");
+	chunks_per_mb = (1 << 20) / chunks_per_mb;
+}
+
+void table_add_header(GtkTable *table, const char *str, int col, int span)
+{
+	GtkWidget *lbl;
+
+	lbl = gtk_label_new(str);
+	gtk_misc_set_alignment(GTK_MISC(lbl), 0.5, 0.5);
+	gtk_table_attach(table, lbl, col, col + span, 0, 1, GTK_FILL, 0, 0, 0);
 }
 
 void init_window(void)
 {
 	GError *err1 = NULL;
 	GError *err2 = NULL;
+	GtkWidget *vbox;
+	GtkWidget *hbox;
+	GtkWidget *table;
 	char *title;
+	const char **header;
 	int x;
 	int y;
+	int i;
 	GdkGeometry hints = {
 		.min_width = 10,
 		.min_height = 10,
@@ -343,8 +430,27 @@ void init_window(void)
 	gtk_window_set_title(GTK_WINDOW(wd), title);
 	g_free(title);
 	gtk_window_set_gravity(GTK_WINDOW(wd), GDK_GRAVITY_STATIC);
+	vbox = gtk_vbox_new(FALSE, 5);
+	hbox = gtk_hbox_new(FALSE, 5);
+	expander = gtk_expander_new("Map");
+	table = gtk_table_new(2, NR_STATS, TRUE);
 	img = gtk_image_new();
-	gtk_container_add(GTK_CONTAINER(wd), img);
+	gtk_container_add(GTK_CONTAINER(wd), vbox);
+	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
+	gtk_box_pack_end(GTK_BOX(vbox), img, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), table, FALSE, FALSE, 0);
+	gtk_box_pack_end(GTK_BOX(hbox), expander, FALSE, FALSE, 0);
+	for (i = 0, header = stats_headings_double; *header != NULL; header++,
+				i += 2)
+		table_add_header(GTK_TABLE(table), *header, i, 2);
+	for (header = stats_headings_single; *header != NULL; header++, i++)
+		table_add_header(GTK_TABLE(table), *header, i, 1);
+	for (i = 0; i < NR_STATS; i++) {
+		label[i] = gtk_label_new(NULL);
+		gtk_misc_set_alignment(GTK_MISC(label[i]), 1, 0.5);
+		gtk_table_attach(GTK_TABLE(table), label[i], i, i + 1, 1, 2,
+					GTK_FILL, 0, 3, 2);
+	}
 	gtk_widget_show_all(GTK_WIDGET(wd));
 	g_signal_connect(wd, "configure-event", G_CALLBACK(configure), wd);
 	g_signal_connect(wd, "delete-event", G_CALLBACK(destroy), NULL);
