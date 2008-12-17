6659ffd4ce1cb447402ed26489d27637d0dfa1be
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2008-12-17 12:52:49 -0500

    tools/readstats: Get state list from nexus.h
    - Get state list from nexus.h rather than hard-coding a list of strings
    
    - Trailing whitespace fixes

--- tools/readstats.c
+++ tools/readstats.c
@@ -1,14 +1,14 @@
 /*
  * readstats - Format and display Nexus state statistics
- * 
+ *
  * Copyright (C) 2006-2008 Carnegie Mellon University
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as published
  * by the Free Software Foundation.  A copy of the GNU General Public License
  * should have been distributed along with this program in the file
  * LICENSE.GPL.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
@@ -16,33 +16,21 @@
  */
 
 #include <stdio.h>
+#include "nexus.h"
 
+#define NEXUS_STATE(x) #x,
 char *states[]={
-	"INVALID",
-	"LOAD_META",
-	"META",
-	"LOAD_DATA",
-	"ENCRYPTED",
-	"DECRYPTING",
-	"CLEAN",
-	"DIRTY",
-	"ENCRYPTING",
-	"DIRTY_ENCRYPTED",
-	"STORE_DATA",
-	"DIRTY_META",
-	"STORE_META",
-	"ERROR_USER",
-	"ERROR_PENDING",
-	"ERROR",
+	NEXUS_STATES
 	NULL
 };
+#undef NEXUS_STATE
 
 int main(int argc, char **argv)
 {
 	char **state=states;
 	FILE *fp;
 	unsigned val;
-	
+
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s file\n", argv[0]);
 		return 1;
