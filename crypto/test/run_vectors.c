2ea2adccd9ec8d8eb7e7fc29fba036c0bddabcd5
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2009-02-03 17:59:16 -0500

    crypto: Update test vectors for hash interface changes

--- crypto/test/run_vectors.c
+++ crypto/test/run_vectors.c
@@ -202,63 +202,91 @@ void monte_test(const char *alg, const struct monte_test *vectors,
 	}
 }
 
-void hash_test(const char *alg, const struct hash_test *vectors,
-			unsigned vec_count, hash_init_fn *init,
-			hash_update_fn *update, hash_final_fn *final,
-			void *ctx, unsigned hashlen)
+void hash_test(const char *alg, enum isrcry_hash type,
+			const struct hash_test *vectors, unsigned vec_count)
 {
+	struct isrcry_hash_ctx *ctx;
 	const struct hash_test *test;
-	uint8_t out[hashlen];
+	uint8_t out[64];
 	unsigned n;
+	unsigned hashlen;
 
+	ctx = isrcry_hash_alloc(type);
+	if (ctx == NULL) {
+		fail("%s alloc", alg);
+		return;
+	}
+	hashlen = isrcry_hash_len(type);
+	if (hashlen < 16 || hashlen > 64)
+		fail("%s invalid hashlen", alg);
 	for (n = 0; n < vec_count; n++) {
 		test = &vectors[n];
-		init(ctx);
-		update(ctx, test->data, test->len);
-		final(ctx, out);
+		isrcry_hash_init(ctx);
+		isrcry_hash_update(ctx, test->data, test->len);
+		isrcry_hash_final(ctx, out);
 		if (memcmp(out, test->hash, hashlen))
 			fail("%s %u result mismatch", alg, n);
 	}
+	isrcry_hash_free(ctx);
 }
 
-void hash_simple_monte_test(const char *alg,
+void hash_simple_monte_test(const char *alg, enum isrcry_hash type,
 			const struct hash_monte_test *vectors,
-			unsigned vec_count, hash_init_fn *init,
-			hash_update_fn *update, hash_final_fn *final,
-			void *ctx, unsigned hashlen)
+			unsigned vec_count)
 {
+	struct isrcry_hash_ctx *ctx;
 	const struct hash_monte_test *test;
-	uint8_t buf[hashlen];
+	uint8_t buf[64];
 	unsigned n;
 	unsigned m;
+	unsigned hashlen;
 
+	ctx = isrcry_hash_alloc(type);
+	if (ctx == NULL) {
+		fail("%s alloc", alg);
+		return;
+	}
+	hashlen = isrcry_hash_len(type);
+	if (hashlen < 16 || hashlen > 64)
+		fail("%s invalid hashlen", alg);
 	for (n = 0; n < vec_count; n++) {
 		test = &vectors[n];
 		memcpy(buf, test->seed, hashlen);
 		if (test->ngroups != 1)
 			fail("%s %u invalid vector", alg, n);
 		for (m = 0; m < test->niters; m++) {
-			init(ctx);
-			update(ctx, buf, hashlen);
-			final(ctx, buf);
+			isrcry_hash_init(ctx);
+			isrcry_hash_update(ctx, buf, hashlen);
+			isrcry_hash_final(ctx, buf);
 		}
 		if (memcmp(buf, test->hash, hashlen))
 			fail("%s %u result mismatch", alg, n);
 	}
+	isrcry_hash_free(ctx);
 }
 
-void hash_monte_test(const char *alg, const struct hash_monte_test *vectors,
-			unsigned vec_count, hash_init_fn *init,
-			hash_update_fn *update, hash_final_fn *final,
-			void *ctx, unsigned hashlen)
+void hash_monte_test(const char *alg, enum isrcry_hash type,
+			const struct hash_monte_test *vectors,
+			unsigned vec_count)
 {
+	struct isrcry_hash_ctx *ctx;
 	const struct hash_monte_test *test;
-	uint8_t buf[3 * hashlen];
-	uint8_t *out = buf + 2 * hashlen;
+	uint8_t buf[192];
+	uint8_t *out;
 	unsigned n;
 	unsigned m;
 	unsigned l;
+	unsigned hashlen;
 
+	ctx = isrcry_hash_alloc(type);
+	if (ctx == NULL) {
+		fail("%s alloc", alg);
+		return;
+	}
+	hashlen = isrcry_hash_len(type);
+	if (hashlen < 16 || hashlen > 64)
+		fail("%s invalid hashlen", alg);
+	out = buf + 2 * hashlen;
 	for (n = 0; n < vec_count; n++) {
 		test = &vectors[n];
 		memcpy(out, test->seed, hashlen);
@@ -266,23 +294,22 @@ void hash_monte_test(const char *alg, const struct hash_monte_test *vectors,
 			for (l = 0; l < 2; l++)
 				memcpy(buf + l * hashlen, out, hashlen);
 			for (l = 0; l < test->niters; l++) {
-				init(ctx);
-				update(ctx, buf, sizeof(buf));
+				isrcry_hash_init(ctx);
+				isrcry_hash_update(ctx, buf, 3 * hashlen);
 				memmove(buf, buf + hashlen, 2 * hashlen);
-				final(ctx, out);
+				isrcry_hash_final(ctx, out);
 			}
 		}
 		if (memcmp(out, test->hash, hashlen))
 			fail("%s %u result mismatch", alg, n);
 	}
+	isrcry_hash_free(ctx);
 }
 
 int main(int argc, char **argv)
 {
 	struct isrcry_aes_key akey;
 	struct isrcry_blowfish_key bfkey;
-	struct isrcry_sha1_ctx sctx;
-	struct isrcry_md5_ctx mctx;
 
 	ecb_test("bf", blowfish_ecb_vectors, MEMBERS(blowfish_ecb_vectors),
 				(init_fn *) isrcry_blowfish_init,
@@ -312,28 +339,14 @@ int main(int argc, char **argv)
 				(cipher_mode_fn *) isrcry_aes_cbc_encrypt,
 				(cipher_mode_fn *) isrcry_aes_cbc_decrypt,
 				&akey, ISRCRY_AES_BLOCKSIZE);
-	hash_test("sha1", sha1_hash_vectors, MEMBERS(sha1_hash_vectors),
-				(hash_init_fn *) isrcry_sha1_init,
-				(hash_update_fn *) isrcry_sha1_update,
-				(hash_final_fn *) isrcry_sha1_final,
-				&sctx, ISRCRY_SHA1_DIGEST_SIZE);
-	hash_monte_test("sha1", sha1_monte_vectors,
-				MEMBERS(sha1_monte_vectors),
-				(hash_init_fn *) isrcry_sha1_init,
-				(hash_update_fn *) isrcry_sha1_update,
-				(hash_final_fn *) isrcry_sha1_final,
-				&sctx, ISRCRY_SHA1_DIGEST_SIZE);
-	hash_test("md5", md5_hash_vectors, MEMBERS(md5_hash_vectors),
-				(hash_init_fn *) isrcry_md5_init,
-				(hash_update_fn *) isrcry_md5_update,
-				(hash_final_fn *) isrcry_md5_final,
-				&mctx, ISRCRY_MD5_DIGEST_SIZE);
-	hash_simple_monte_test("md5", md5_monte_vectors,
-				MEMBERS(md5_monte_vectors),
-				(hash_init_fn *) isrcry_md5_init,
-				(hash_update_fn *) isrcry_md5_update,
-				(hash_final_fn *) isrcry_md5_final,
-				&mctx, ISRCRY_MD5_DIGEST_SIZE);
+	hash_test("sha1", ISRCRY_HASH_SHA1, sha1_hash_vectors,
+				MEMBERS(sha1_hash_vectors));
+	hash_monte_test("sha1", ISRCRY_HASH_SHA1, sha1_monte_vectors,
+				MEMBERS(sha1_monte_vectors));
+	hash_test("md5", ISRCRY_HASH_MD5, md5_hash_vectors,
+				MEMBERS(md5_hash_vectors));
+	hash_simple_monte_test("md5", ISRCRY_HASH_MD5, md5_monte_vectors,
+				MEMBERS(md5_monte_vectors));
 
 	if (failed) {
 		printf("%d tests failed\n", failed);
