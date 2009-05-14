e7aecbd7959dce16507a2a21504a99a9869b1106
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2009-05-14 14:45:00 -0400

    crypto: Use universal {LOAD,STORE}32{H,L} built on glib byteswap macros
    - Replace assembly-optimized and generic {LOAD,STORE}32{H,L} macros
    with new generic ones using the glib byte-swap macros.  The new macros
    generate better code than the original assembly-optimized ones, and are
    more readable than the macros they replace.  This change also fixes a
    miscompilation of the Blowfish code under -O3 by gcc 4.4, which occurs
    because the constraint specifications on the inline assembly are
    incorrect (they do not specify memory dependencies/clobbers).

--- crypto/internal.h
+++ crypto/internal.h
@@ -164,45 +164,35 @@ struct isrcry_sign_ctx {
 
 #if defined(HAVE_X86_32) || defined(HAVE_X86_64)
 #define ISRCRY_FAST_TYPE unsigned long
-#define STORE32H(x, y)           \
-asm __volatile__ (               \
-   "bswapl %0     \n\t"          \
-   "movl   %0,(%1)\n\t"          \
-   "bswapl %0     \n\t"          \
-      ::"r"(x), "r"(y));
-
-#define LOAD32H(x, y)          \
-asm __volatile__ (             \
-   "movl (%1),%0\n\t"          \
-   "bswapl %0\n\t"             \
-   :"=r"(x): "r"(y));
-
-#define STORE32L(x, y) \
-	{ uint32_t __t = (x); memcpy((y), &__t, 4); }
-
-#define LOAD32L(x, y) \
-	memcpy(&(x), (y), 4)
-#else
-#define STORE32H(x, y)                                                                     \
-     { (y)[0] = (unsigned char)(((x)>>24)&255); (y)[1] = (unsigned char)(((x)>>16)&255);   \
-       (y)[2] = (unsigned char)(((x)>>8)&255); (y)[3] = (unsigned char)((x)&255); }
-
-#define LOAD32H(x, y)                            \
-     { x = ((unsigned long)((y)[0] & 255)<<24) | \
-           ((unsigned long)((y)[1] & 255)<<16) | \
-           ((unsigned long)((y)[2] & 255)<<8)  | \
-           ((unsigned long)((y)[3] & 255)); }
-
-#define STORE32L(x, y)                                                                     \
-     { (y)[3] = (unsigned char)(((x)>>24)&255); (y)[2] = (unsigned char)(((x)>>16)&255);   \
-       (y)[1] = (unsigned char)(((x)>>8)&255); (y)[0] = (unsigned char)((x)&255); }
-
-#define LOAD32L(x, y)                            \
-     { x = ((unsigned long)((y)[3] & 255)<<24) | \
-           ((unsigned long)((y)[2] & 255)<<16) | \
-           ((unsigned long)((y)[1] & 255)<<8)  | \
-           ((unsigned long)((y)[0] & 255)); }
 #endif
+
+#define STORE32H(x, y)				\
+	do {					\
+		uint32_t __t = (x);		\
+		__t = GUINT32_TO_BE(__t);	\
+		memcpy((y), &__t, 4);		\
+	} while (0)
+
+#define LOAD32H(x, y)				\
+	do {					\
+		uint32_t __t;			\
+		memcpy(&__t, (y), 4);		\
+		x = GUINT32_FROM_BE(__t);	\
+	} while (0)
+
+#define STORE32L(x, y)				\
+	do {					\
+		uint32_t __t = (x);		\
+		__t = GUINT32_TO_LE(__t);	\
+		memcpy((y), &__t, 4);		\
+	} while (0)
+
+#define LOAD32L(x, y)				\
+	do {					\
+		uint32_t __t;			\
+		memcpy(&__t, (y), 4);		\
+		x = GUINT32_FROM_LE(__t);	\
+	} while (0)
 
 enum isrcry_result isrcry_gen_prime(mpz_t out, struct isrcry_random_ctx *rctx,
 			unsigned len);
