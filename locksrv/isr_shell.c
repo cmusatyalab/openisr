507a30a134bd78d03040bda56c154a264c30ca09
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2009-11-24 16:30:15 -0500

    locksrv/isr_shell: Allow the user to run rsync
    - Allow the user to run rsync so that sync/checkin will work
    
    - Make the command filter table-driven rather than open-coding each case

--- locksrv/isr_shell.c
+++ locksrv/isr_shell.c
@@ -21,6 +21,17 @@
 #include <unistd.h>
 #include <glib.h>
 
+const struct command {
+	const char *name;
+	int nargs;		/* -1 if variable */
+} commands[] = {
+	{"isr_runserv", -1},
+	{"rsync", -1},
+	{"ssh-add", 0},
+	{"passwd", 0},
+	{NULL, 0}		/* must be last! */
+};
+
 static void die(const char *msg)
 {
 	fprintf(stderr, "%s\n", msg);
@@ -31,6 +42,7 @@ int main(int argc, char **argv)
 {
 	int sub_argc;
 	char **sub_argv;
+	const struct command *cmd;
 
 	if (argc != 3 || strcmp(argv[1], "-c"))
 		die("This is not an interactive shell.");
@@ -39,14 +51,12 @@ int main(int argc, char **argv)
 				sub_argc == 0)
 		die("Couldn't parse command.");
 
-	if (!strcmp(sub_argv[0], "passwd") ||
-				!strcmp(sub_argv[0], "ssh-add")) {
-		if (sub_argc != 1)
-			die("You may not pass extra arguments to this "
-						"command.");
-	} else if (strcmp(sub_argv[0], "isr_runserv")) {
+	for (cmd = commands; cmd->name != NULL &&
+				strcmp(cmd->name, sub_argv[0]); cmd++);
+	if (cmd->name == NULL)
 		die("You may not run arbitrary commands on this system.");
-	}
+	if (cmd->nargs != -1 && cmd->nargs != sub_argc - 1)
+		die("Incorrect number of arguments for command.");
 
 	execvp(sub_argv[0], sub_argv);
 	die("Couldn't run command.");
