cmusatyalab/openisr

commit 2f8641b64bd4d7cd7c0480325dee6802b75b605d

crypto: Convert lengths in cipher API to unsigned int
- The cipher API used unsigned long for length parameters, in contrast
to the rest of the library which uses unsigned int.  This caused problems
when a length outparam stored by isrcry_cipher_final() was then passed to
e.g. isrcry_compress_final().  Convert cipher API to unsigned int length
parameters for consistency.

- Update callers

tools/disktool.c
@@ -89,7 +89,7 @@ static struct isrcry_compress_ctx *compress_ctx;
 
 static struct db *sqlitedb;
 
-static gulong chunklen;
+static unsigned chunklen;
 static gpointer zerodata;
 static gpointer tmpdata;
 
@@ -347,7 +347,7 @@ struct chunk_desc {
 	gpointer tag;
 	gpointer key;
 	gpointer data;
-	gulong len;
+	unsigned len;
 	unsigned int compression;
 };
 
@@ -437,7 +437,6 @@ static void read_chunk(unsigned int idx, struct chunk_desc *chunk)
 	GString *dest;
 	int fd;
 	enum isrcry_result rc;
-	gulong tmplen;
 	unsigned inlen;
 	unsigned outlen;
 
@@ -472,9 +471,9 @@ static void read_chunk(unsigned int idx, struct chunk_desc *chunk)
 		return;
 	}
 
-	tmplen = chunklen;
+	outlen = chunklen;
 	rc = isrcry_cipher_final(cipher_ctx, padding, chunk->data, chunk->len,
-				 tmpdata, &tmplen);
+				 tmpdata, &outlen);
 	if (rc)
 		die("Failed to decrypt compressed chunk: %s",
 		    isrcry_strerror(rc));
@@ -497,7 +496,7 @@ static void read_chunk(unsigned int idx, struct chunk_desc *chunk)
 	if (rc)
 		die("Failed to initialize decompressor: %s",
 					isrcry_strerror(rc));
-	inlen = tmplen;
+	inlen = outlen;
 	outlen = chunklen;
 	rc = isrcry_compress_final(compress_ctx, tmpdata, &inlen,
 				chunk->data, &outlen);

crypto/cbc.c
@@ -42,7 +42,7 @@
   @return ISRCRY_OK if successful
 */
 static enum isrcry_result cbc_encrypt(struct isrcry_cipher_ctx *cctx,
-			const unsigned char *in, unsigned long len,
+			const unsigned char *in, unsigned len,
 			unsigned char *out)
 {
    unsigned blocklen = cctx->cipher->blocklen;
@@ -103,7 +103,7 @@ static enum isrcry_result cbc_encrypt(struct isrcry_cipher_ctx *cctx,
   @return ISRCRY_OK if successful
 */
 static enum isrcry_result cbc_decrypt(struct isrcry_cipher_ctx *cctx,
-			const unsigned char *in, unsigned long len,
+			const unsigned char *in, unsigned len,
 			unsigned char *out)
 {
    unsigned blocklen = cctx->cipher->blocklen;

crypto/cipher.c
@@ -100,7 +100,7 @@ exported enum isrcry_result isrcry_cipher_init(struct isrcry_cipher_ctx *cctx,
 
 exported enum isrcry_result isrcry_cipher_process(
 			struct isrcry_cipher_ctx *cctx, const void *in,
-			unsigned long inlen, void *out)
+			unsigned inlen, void *out)
 {
 	if (cctx->direction == ISRCRY_ENCRYPT)
 		return cctx->mode->encrypt(cctx, in, inlen, out);
@@ -111,7 +111,7 @@ exported enum isrcry_result isrcry_cipher_process(
 exported enum isrcry_result isrcry_cipher_final(
 			struct isrcry_cipher_ctx *cctx,
 			enum isrcry_padding padding, const void *in,
-			unsigned long inlen, void *out, unsigned long *outlen)
+			unsigned inlen, void *out, unsigned *outlen)
 {
 	const struct isrcry_pad_desc *desc;
 	enum isrcry_result ret;

crypto/ecb.c
@@ -20,7 +20,7 @@
 #include "internal.h"
 
 static enum isrcry_result ecb_encrypt(struct isrcry_cipher_ctx *cctx,
-			const unsigned char *in, unsigned long len,
+			const unsigned char *in, unsigned len,
 			unsigned char *out)
 {
 	enum isrcry_result ret;
@@ -41,7 +41,7 @@ static enum isrcry_result ecb_encrypt(struct isrcry_cipher_ctx *cctx,
 }
 
 static enum isrcry_result ecb_decrypt(struct isrcry_cipher_ctx *cctx,
-			const unsigned char *in, unsigned long len,
+			const unsigned char *in, unsigned len,
 			unsigned char *out)
 {
 	enum isrcry_result ret;

crypto/internal.h
@@ -54,10 +54,10 @@ extern const struct isrcry_cipher_desc _isrcry_bf_desc;
 
 struct isrcry_mode_desc {
 	enum isrcry_result (*encrypt)(struct isrcry_cipher_ctx *cctx,
-				const unsigned char *in, unsigned long len,
+				const unsigned char *in, unsigned len,
 				unsigned char *out);
 	enum isrcry_result (*decrypt)(struct isrcry_cipher_ctx *cctx,
-				const unsigned char *in, unsigned long len,
+				const unsigned char *in, unsigned len,
 				unsigned char *out);
 };
 

crypto/isrcrypto.h
@@ -114,7 +114,7 @@ enum isrcry_result isrcry_cipher_init(struct isrcry_cipher_ctx *cctx,
    be a multiple of the block length.  @out must be able to store @inlen
    bytes. */
 enum isrcry_result isrcry_cipher_process(struct isrcry_cipher_ctx *cctx,
-			const void *in, unsigned long inlen, void *out);
+			const void *in, unsigned inlen, void *out);
 
 /* Process the final cipher block, padding appropriately with the specified
    @padding.  This function should not be called if the application wishes
@@ -125,9 +125,8 @@ enum isrcry_result isrcry_cipher_process(struct isrcry_cipher_ctx *cctx,
    isrcry_cipher_init() or used for further cipher operations with
    isrcry_cipher_process()/isrcry_cipher_final(). */
 enum isrcry_result isrcry_cipher_final(struct isrcry_cipher_ctx *cctx,
-			enum isrcry_padding padding,
-			const void *in, unsigned long inlen,
-			void *out, unsigned long *outlen);
+			enum isrcry_padding padding, const void *in,
+			unsigned inlen, void *out, unsigned *outlen);
 
 /* Return the cipher block length for the given cipher, in bytes. */
 unsigned isrcry_cipher_block(enum isrcry_cipher type);

crypto/test/run_vectors.c
@@ -141,7 +141,7 @@ void chain_pad_test(const char *alg, enum isrcry_cipher type,
 	const struct chain_test *test;
 	enum isrcry_result ret;
 	unsigned char buf[1024];
-	unsigned long outlen;
+	unsigned outlen;
 	unsigned n;
 	unsigned blocksize;
 
@@ -168,7 +168,7 @@ void chain_pad_test(const char *alg, enum isrcry_cipher type,
 			fail("%s %u encrypt %d", alg, n, ret);
 		if (outlen != test->plainlen + (blocksize -
 					(test->plainlen % blocksize)))
-			fail("%s %u encrypt invalid len %lu", alg, n, outlen);
+			fail("%s %u encrypt invalid len %u", alg, n, outlen);
 		if (memcmp(buf, test->cipher, outlen))
 			fail("%s %u encrypt mismatch", alg, n);
 

tools/blobtool.c
@@ -388,7 +388,7 @@ static void run_cipher(const char *in, unsigned inlen, GString *out,
 	static unsigned offset;
 	char finalbuf[2 * cipher_block];
 	unsigned count;
-	unsigned long outlen;
+	unsigned outlen;
 	enum isrcry_result ret;
 
 	if (ctx == NULL) {

commit 97bc82fcb081ea17cef8120ae1cff766fa068ee2

tools/disktool: Fix progress bar when exporting large parcels
- Fix missing/wrong progress bar when exporting parcels > 4 GB
(introduced in 2f8641b6)

tools/disktool.c
@@ -693,7 +693,7 @@ static void export_image(const gchar *img)
 		exit(1);
 	}
 
-	init_progress(nchunk * chunklen);
+	init_progress((off_t)nchunk * chunklen);
 
 	/* if the image is a disk or pipe ftruncate will fail and we should
 	 * write out zero blocks */