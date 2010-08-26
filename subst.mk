769e7b91858c22f09b36b16b1354b16ddc42518f
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2010-08-26 00:12:51 -0400

    Remove mechanisms for building and loading kernel modules
    - Remove openisr-config command for building kernel modules
    
    - Remove initscript for loading (and, if necessary, building) the
    kernel modules at boot
    
    - No longer run openisr-config and enable the initscript during postinst

--- subst.mk
+++ subst.mk
@@ -9,7 +9,6 @@ $(GEN): %: %.in Makefile $(REVISION_DEPENDS)
 				-e "s=!!SYSCONFDIR!!=$(pkgsysconfdir)=g" \
 				-e "s=!!BINDIR!!=$(bindir)=g" \
 				-e "s=!!SBINDIR!!=$(sbindir)=g" \
-				-e "s=!!INITDIR!!=$(initexecdir)=g" \
 				-e "s=!!STATEDIR!!=$(pkgstatedir)=g" \
 				-e "s=!!PERLPATH!!=$(PERL)=g" \
 				-e "s=!!PYTHONPATH!!=$(PYTHON)=g" \
